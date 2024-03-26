d72862c540c5911952a4e468ada799132929fdde
brinfoleo <brinfo.leo@gmail.com>
2024-03-26 13:58:05 +0000

    second update

--- Token.cs
+++ Token.cs
@@ -7,21 +7,25 @@ using Microsoft.IdentityModel.Tokens;
 public static class Token
 {
 
-
     public static object Create(string _key, string role)
     {
         var key = Encoding.ASCII.GetBytes(_key);
-        var tokenConfig = new SecurityTokenDescriptor
+
+        var tokenescriptor = new SecurityTokenDescriptor
         {
             Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
             {
                 new Claim(ClaimTypes.Role, role)
             }),
+            
             Expires = DateTime.UtcNow.AddHours(3),
-            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key),
+             SecurityAlgorithms.HmacSha256Signature)
         };
+
         var tokenHandler = new JwtSecurityTokenHandler();
-        var token = tokenHandler.CreateToken(tokenConfig);
+        var token = tokenHandler.CreateToken(tokenescriptor);
         var torkenString = tokenHandler.WriteToken(token);
 
         return new
